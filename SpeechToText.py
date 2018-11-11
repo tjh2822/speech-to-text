cd8f6356517c6b2355e0f9e56e9a8e8c6ca0084e
Thomas J. Hall <tjh2822@g.rit.edu>
2018-11-11 14:02:07 -0400

    added mic

--- SpeechToText.py
+++ SpeechToText.py
@@ -6,18 +6,31 @@ def main():
     print("Python Speech To Text via speech_recognition\n Speech Recognition Version:" + str(sr.__version__) )
 
     r = sr.Recognizer()
-    mic = sr.Microphone(device_index=3)
+    mic = sr.Microphone(device_index=1)
+
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(r.recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(mic, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
     
     #source file = sr.AudioFile('harvard.wav')
 
+    # Set Ambient Noise
+    with mic as source:
+        r.recognizer.adjust_for_ambient_noise(source)
+        audio = r.recognizer.listen(source)
+      
+    # Try recording the audio sample
     recording=""
     with mic as source:
         print("Recording... Speak Now")
         r.adjust_for_ambient_noise(source, duration=0.5)
-        recording = r.record(source)
+        recording = r.record(source duration=4)
     
     results = r.recognize_google(recording)
-    print("Results")
+    print("Results" + str(results))
 
 if __name__ == '__main__':
     main()
