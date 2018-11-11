183f5cf23a962c33abb6c4950c7530d3493e70df
Thomas J. Hall <tjh2822@g.rit.edu>
2018-11-11 12:54:21 -0400

    added recording

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,16 +1,23 @@
 import speech_recognition as sr
+import pyaudio
 
 def main():
     # Print the SR Version and program data
     print("Python Speech To Text via speech_recognition\n Speech Recognition Version:" + str(sr.__version__) )
 
     r = sr.Recognizer()
-    harvard = sr.AudioFile('harvard.wav')
+    mic = sr.Microphone(device_index=3)
+    
+    #source file = sr.AudioFile('harvard.wav')
 
     recording=""
-    with harvard as source:
+    with mic as source:
+        print("Recording... Speak Now")
         r.adjust_for_ambient_noise(source, duration=0.5)
-        recording= = r.record(source)
+        recording = r.record(source)
+    
+    results = r.recognize_google(recording)
+    print("Results")
 
 if __name__ == '__main__':
     main()
